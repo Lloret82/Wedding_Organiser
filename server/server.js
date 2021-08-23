11a6b7edfe954e9cb2794dec5ae4b100d22ff2c3
lloret82 <adbmanchester@gmail.com>
2021-08-23 15:55:55 +0100

    heroku

--- server/server.js
+++ server/server.js
@@ -1,46 +1,45 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const cors = require ("cors")
-const path = require('path')
-
+const bodyParser = require("body-parser");
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   // context: authMiddleware,
 });
+require("dotenv").config()
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+    mongoose
+     .connect(
+         process.env.MONGODB_CONNECTION_STRING,
+             {
+               useNewUrlParser: true,
+               useUnifiedTopology: true,
+             }
+     )
+     .then(() => console.log("MongoDB has been connected"))
+     .catch((err) => console.log(err));
 
 server.applyMiddleware({ app })
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
-
-
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
-
-
-
-
 
 db.once('open', () => {
   app.listen(PORT, () => {
